2dbfc752f92f86e7b2af880820807feaf95c2c48
markli <markl@wicresoft.com>
2024-07-01 21:33:22 +0800

    initial

---
+++ main/java/cn/itcast/client/NettyClient.java
@@ -0,0 +1,46 @@
+package cn.itcast.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class NettyClient {
+
+    private final String host;
+    private final int port;
+
+    public NettyClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void connect() throws InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(host, port)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            // 添加自定义的处理器(handler)以处理入站和出站数据
+                        }
+                    });
+
+            ChannelFuture f = b.connect().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        NettyClient client = new NettyClient("127.0.0.1", 8080); // 替换为另一个服务端的IP和端口
+        client.connect();
+    }
+}
